879b7679d391107762f7550f69e741bad9fef7c8
InbarMargalitBarkai <inbarbarkai6@gmail.com>
2022-05-27 00:29:51 +0300

    fix: user data

--- chatApp/webServerTask/Models/User.cs
+++ chatApp/webServerTask/Models/User.cs
@@ -1,72 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace webServerTask.Models
+﻿namespace webServerTask.Models
 {
     public class User
     {
-        private int Id;
-        private string Username;
-        private string Nickname;
-        private string Password;
-        private List<Conversation> Conversations;
-        private List<User> Contacts;
-
-        public User() { }
-
-        public User(int Id, string Username, string Nickname, string Password,
-            List<Conversation>? Conversations = null, List<User>? Contacts = null)
-        {
-            this.Id = Id;
-            this.Username = Username;
-            this.Nickname = Nickname;
-            this.Password = Password;
-            this.Conversations = Conversations;
-            this.Contacts = Contacts;
-
-            if (this.Id <= 0)
-            {
-                throw new ArgumentException(
-                    "The Id has to be positive real number.");
-            }
-
-            if (Conversations == null)
-            {
-                Conversations = "No description.";
-            }
-
-            this.description = description;
-
-            if (imagePath == null)
-            {
-                imagePath = "default.png";
-            }
-
-            this.imagePath = imagePath;
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public double Price
-        {
-            get { return price; }
-            set { price = value; }
-        }
-
-        public string Description
-        {
-            get { return description; }
-            set { description = value; }
-        }
-
-        public string ImagePath
-        {
-            get { return imagePath; }
-            set { imagePath = value; }
-        }
-
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public List<Conversation> Conversations { get; set; }
+        public List<User> Contacts { get; set; }
     }
 }
